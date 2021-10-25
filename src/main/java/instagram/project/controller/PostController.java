436e88afe4d3c194661bce8788c0125f6569965d
unknown <nguyenvantu11041999@gmail.com>
2021-10-25 16:45:27 +0700

    try

--- src/main/java/instagram/project/controller/PostController.java
+++ src/main/java/instagram/project/controller/PostController.java
@@ -1,11 +1,16 @@
 package instagram.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import instagram.project.entity.UserInstagram;
+import instagram.project.repository.UserInstagramRepository;
 import instagram.project.response.PagedResponse;
 import instagram.project.response.PostInstagramResponse;
 import instagram.project.service.PostService;
@@ -17,10 +22,20 @@ public class PostController {
 	@Autowired
 	private PostService postService;
 	
+	@Autowired
+	private UserInstagramRepository userInstagramRepository;
+	
 	@GetMapping
 	public PagedResponse<PostInstagramResponse> getAllPhotos(
 			@RequestParam(name = "page", required = false, defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) Integer page,
 			@RequestParam(name = "size", required = false, defaultValue = AppConstants.DEFAULT_PAGE_SIZE) Integer size) {
 		return postService.getAllPosts(page, size);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> createUserIns(){
+		userInstagramRepository.save(new UserInstagram(0, "van tu","vantu", null));
+		return ResponseEntity.ok().build();
+		
 	}
 }
