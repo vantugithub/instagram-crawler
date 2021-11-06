9973e89fa32de4d96230753196d88d9ce10dbe23
unknown <nguyenvantu11041999@gmail.com>
2021-11-06 13:23:20 +0700

    update auth for web

--- src/main/java/instagram/project/controller/PostController.java
+++ src/main/java/instagram/project/controller/PostController.java
@@ -1,6 +1,7 @@
 package instagram.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import instagram.project.utils.AppConstants;
 
 @RestController
 @RequestMapping("/api/posts")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PostController {
 	@Autowired
 	private PostService postService;
