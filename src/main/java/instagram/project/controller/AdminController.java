ac890e6bdcc59f5bf9e7e2aa19c4992974199b5a
unknown <nguyenvantu11041999@gmail.com>
2021-12-27 00:44:25 +0700

    that tinh

--- src/main/java/instagram/project/controller/AdminController.java
+++ src/main/java/instagram/project/controller/AdminController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,8 +51,10 @@ public class AdminController {
 	
 	}
 	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	@PutMapping("/topics/{id}")
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@RequestMapping(value = "/topics/{id}", 
+			  produces = "application/json", 
+			  method=RequestMethod.PUT)
 	public ResponseEntity updateTopic(@RequestParam(name = "id", required = true) int id) {
 		
 		if(!topicService.existsById(id)) {
